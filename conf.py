6606472c43a487efcc795de6478772a348e79c26
Till Hoffmann <tillahoffmann@gmail.com>
2024-03-22 17:44:54 -0400

    Add sphinx documentation.

---
+++ conf.py
@@ -0,0 +1,11 @@
+project = "numpyro-extensions"
+html_theme = "sphinx_book_theme"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.napoleon",
+]
+exclude_patterns = [
+    "venv",
+]
+add_module_names = False
---
+++ index.rst
@@ -0,0 +1,8 @@
+numpyro-extensions
+==================
+
+Experimental extensions for `numpyro <https://github.com/pyro-ppl/numpyro>`__.
+
+.. toctree::
+
+    docs/distributions/transforms
