86ec7b245f92eff7f35d6679e4264799f95bec97
Till Hoffmann <tillahoffmann@gmail.com>
2024-03-25 17:45:08 -0400

    Add intersphinx configuration.

--- conf.py
+++ conf.py
@@ -3,9 +3,16 @@ html_theme = "sphinx_book_theme"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
 ]
 exclude_patterns = [
     "venv",
 ]
 add_module_names = False
+intersphinx_mapping = {
+    "jax": ("https://jax.readthedocs.io/en/latest/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "numpyro": ("https://num.pyro.ai/en/stable/", None),
+    "python": ("https://docs.python.org/3", None),
+}
